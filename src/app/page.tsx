7cbc0330fa358919bd1d21d87a08f5a77961266e
USER01\kanno <kanno062316@icloud.com>
2024-07-21 12:47:20 +0900

    取得、登録APIの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
-import type { NextPage } from "next";
+import { TodoType } from "@/types";
+import Link from "next/link";
 
-const Home: NextPage = () => {
+async function fetchAllTodos() {
+  const res = await fetch(`http://localhost:3000/api/todo`, {
+    //SSR(更新があるため)
+    cache: "no-store",
+  });
+  const data = await res.json();
+  return data.todos;
+}
+
+export default async function Home() {
+  const todos = await fetchAllTodos();
   return (
-    <>
-      <h2>hello world!!</h2>
-    </>
-  );
-};
+    <div className="flex justify-center">
+      {todos.map((post: TodoType) => (
+        <div
+          key={post.id}
+          className="w-1/2 p-4 rounded-md mx-3 my-2 bg-slate-300 flex justify-center"
+        >
+          <div className="w-1/4">
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">タスク名：{post.title}</h2>
+              </div>
+            </div>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
+          </div>
 
-export default Home;
+          <div className="w-3/4 my-3">
+            <h2>{post.content}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
